2373c77a668002c06c770b046c7f6112b835370e
Alejandra Reynoso <alejandrareinosoh@gmail.com>
2022-02-19 18:23:08 -0700

    Calculator

---
+++ calc.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+const { ReadableStreamBYOBRequest } = require('stream/web');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+	console.log('Operator', mathSymbol);
+	if (mathSymbol == "+"){
+		console.log(num1 + num2);
+  }
+  if (mathSymbol == "-"){
+	console.log(num1 - num2);
+}
+if (mathSymbol == "*"){
+	console.log(num1 * num2);
+}
+if (mathSymbol == "/"){
+	console.log(num1 / num2);
+}
+if (mathSymbol == "sqrt"){
+	console.log(Math.sqrt(num1));
+}
+
+});
